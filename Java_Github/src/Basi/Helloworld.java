cff65d5e5138a3350396dd62889dadba485ebcb4
Prem G <Prem G@DESKTOP-BN6LRU6>
2020-12-03 19:58:13 +0530

    Hello-World

---
+++ Java_Github/src/Basi/Helloworld.java
@@ -0,0 +1,17 @@
+package Basi;
+
+class World
+{
+	{
+		System.out.println("Helloworld");
+	}
+}
+public class Helloworld {
+
+	public static void main(String args[])
+	{
+		World ref = new World();
+		
+		
+	}
+}
