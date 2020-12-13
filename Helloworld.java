4c3b10ea21553fbf20fe2a4b7a761525d52457b0
premkumar G <63385985+creativecstudios@users.noreply.github.com>
2020-12-13 07:46:12 +0530

    Changing package Basi to Basic

---
+++ Helloworld.java
@@ -0,0 +1,12 @@
+package Basics;
+
+public class Helloworld {
+
+	public static void main(String args[])
+	{
+		Print ref = new Print();
+		Calculator ref1 = new Calculator();
+		ref1.average(2,4,6);
+		ref1.average(2,4,6,4,7);
+	}
+}
