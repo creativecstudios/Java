fc46dd2132e6cab67946061c822077072cc88a18
premkumar G <63385985+creativecstudios@users.noreply.github.com>
2020-12-14 21:16:58 +0530

    Updated code for Lcm

--- Helloworld.java
+++ Helloworld.java
@@ -1,12 +1,15 @@
 package Basics;
 
-public class Helloworld {
+import java.util.Arrays;
 
-	public static void main(String args[])
+public class Helloworld {
+	 public static void main(String args[])
 	{
 		Print ref = new Print();
 		Calculator ref1 = new Calculator();
 		ref1.average(2,4,6);
 		ref1.average(2,4,6,4,7);
+		ref1.highestCommonFactor(3,6,5);
+		ref1.leastCommonMultiple(125,250,620);
 	}
 }
