e4bd413b7ef74d201b57c72f1dcdb5a82f414c9e
premkumar G <63385985+creativecstudios@users.noreply.github.com>
2020-12-16 17:46:04 +0530

    Added files with src

---
+++ src/Basics/Calculator.java
@@ -0,0 +1,81 @@
+package Basics;
+
+import java.util.Arrays;
+
+public class Calculator {
+	
+	protected int average(int num1,int num2, int num3)
+	{
+		int result = ((num1+num2+num3)/3);
+		System.out.println("The average of 3 num is "+ result);
+		return result;
+	}
+	//variable parameter
+    protected int average(int...num1)
+	{
+		 int count=0;
+		 int sum=0;
+		for(int i:num1)
+		{
+			sum = sum+i;
+			count++;
+		}
+		int result = sum/count;
+		System.out.println("The average of n numbers is "+result);
+		return(result);
+	}
+    protected int highestCommonFactor(int...inputs)
+	{
+		int result = 0;
+		for(int i=0; i<inputs.length-1; i++)
+		{
+			while(inputs[i+1]>0)
+			{
+				int temp = inputs[i+1];
+				inputs[i+1]= inputs[i]%inputs[i+1];
+				inputs[i]= temp;
+				result = inputs[i];
+			}
+			inputs[i+1]= result;
+		}
+		System.out.println("The Highest Common Factor is "+result);
+		return result;
+	}
+   protected int leastCommonMultiple(int...inputs)
+    {
+    	int max,min,x,lcm=0;
+    	for(int i=0; i<inputs.length; i++)
+    	{
+    		for(int j=i+1; j<inputs.length-1; j++)
+    		{
+    		   if(inputs[i]>=inputs[j])
+    			{
+    				max = inputs[i];
+    				min = inputs[j];
+    			}
+    			else
+    			{
+    				max = inputs[j];
+    				min = inputs[i];
+    			}
+    			for(int k=0; k<inputs.length; k++)
+    			{
+    				if(max!=0 && min!=0)
+    				{
+    					x = k*max;
+        				if(x%min==0)
+        				{
+        					lcm = x;
+        				}
+    				}
+    			}
+    		}
+    	}
+    	System.out.println("The Least Common Multiple is "+lcm);
+    	return lcm;
+    }
+}
+
+
+
+
