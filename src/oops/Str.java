a0d28ccef09ee300c97a4b4c0d866158af6f71f1
premkumar G <63385985+creativecstudios@users.noreply.github.com>
2021-01-25 12:09:17 +0530

    Add files via upload
    Added some string methods

--- src/oops/Str.java
+++ src/oops/Str.java
@@ -1,31 +1,21 @@
 package oops;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 public class Str {
 	
 	
 	//user input the string
-	void userinput() {
+	public void userinput() {
 		Scanner z = new Scanner(System.in);
 		System.out.println("Enter the string");
 		String s = z.next();
 		System.out.println(s);
 	}
 	
-	
-	//finding the character
-	void finding() {
-		String s = "Umberlla";
-		for(int i=0; i<s.length(); i++){
-			if(s.charAt(i)=='e') {
-				System.out.println(i+"the character is"+s.charAt(i));
-			}
-		}
-	}
-	
-	
-	//to check the word e is in umberlla
-	void checking() {
+	//to check the word e is in umbrella
+	public void checking() {
 		String s = "Umberlla";
 		for(int i=0;i<s.length();i++) {
 			if(s.charAt(i)=='e') {
@@ -37,9 +27,8 @@ public class Str {
 		}
 	}
 	
-	
 	//checking the word in the sentence
-	void wordcheck() {
+	public void wordcheck() {
 		String s = "This is a word";
 		if(s.contains("word")) {
 			System.out.println("true");
@@ -51,7 +40,7 @@ public class Str {
 	
 	
 	//Print the name ex P.R.lastword
-	void nameacronym() {
+	public void nameacronym() {
 		String name = "Robert Jonn Britto";
 		String A = ""+name.charAt(0)+".";
 		for(int i=0; i<name.length(); i++) {
@@ -75,10 +64,11 @@ public class Str {
 	
 	
 	//find the number of vowels
-	String findvowels() {
+	public String findvowels() {
 		int vowels=0,consonants=0,numbers=0,spaces=0,symbols=0;
 		String s = "This is Premganesh form Ece department";
 		s = s.toLowerCase();
+		
 		for(int i=0; i<s.length(); i++) {
 			char ch = s.charAt(i);
 			if(ch == 'a' || ch=='e'||ch=='i'||ch=='o'||ch=='u') {
@@ -104,6 +94,111 @@ public class Str {
 		System.out.println("spaces = "+spaces);
 		System.out.println("symbols = "+symbols);
 		return s;
+	}
+	
+	
+	//Palindrome
+	public void palindrome() {
+		String s = "Madam";
+		StringBuffer sb = new StringBuffer(s);
+		sb = sb.reverse();
+		String T = sb.toString();
+		if(s.equalsIgnoreCase(T)) {
+			System.out.println("Palindrome");
+		}
+		else {
+			System.out.println("Not a palindrome");
+		}
+	}
+
+	
+	//Finding Largest and Smallest word in the string
+	public String LargeSmall() {
+		String s = "A set of words that is complete is itself";
+		s = s.toLowerCase()+" ";
+		
+		String word="";
+		String[] words = new String[100];
+		int length=0;
+		
+		for(int i=0; i<s.length(); i++) {
+			if(s.charAt(i)!=' ') {
+				word = word+s.charAt(i);
+			}
+			else {
+				words[length] = word;
+				length++;
+				word ="";
+			}
+		}
+		
+		String small="",large="";
+		small = large = words[0];
+		
+		for(int j=0; j<length; j++) {
+			if(small.length()>words[j].length()) {
+				small = words[j];
+			}
+			if(large.length()<words[j].length()) {
+				large = words[j];
+			}
+		}
+		System.out.println(s);
+		System.out.println("Smallest  "+small);
+		System.out.println("Largest word  "+large);
+		return s;
+	}
+	
+	
+	//array ascending order
+	void array() {
+		String arr[] = {"prem","kumar","ranjith"};
+		Arrays.sort(arr);
+		System.out.println(Arrays.toString(arr));	
+	}
+	
+	//palindrome replaces *
+	void palindromestar() {
+		String s = "madam how are you what about radar.";
+		s = s+" ";
+		String rev="";
+		String word ="";
+		String array[] = new String[100];
+		int length=0;
+		for(int i=0; i<s.length(); i++) {
+			if(s.charAt(i)!=' ') {
+				word = word+s.charAt(i);
+			}
+			else
+			{
+				array[length] = word;
+				length++;
+				StringBuffer obj = new StringBuffer(word);
+				obj.reverse();
+				rev = obj.toString();
+				if(rev.equalsIgnoreCase(word)) {
+					for(int j=0; j<rev.length(); j++) {
+						System.out.print("*");
+					}
+					System.out.print(" ");
+				}
+				else {
+					System.out.print(word+" ");
+				}
+				word = "";
+			}
+			
+		}
+	}
+
+	//replace string
+	void replacing() {
+		String s = "A batman with ball";
+		if(s.contains("ball"))
+		{
+			s.replaceAll("ball", "bat");
+		}
+		
 	}
 }
 
